48ccae1fa81b18e41d1e0346ffe59ce86b01b4e7
johame72 <johnhmerkel@gmail.com>
2024-02-15 22:50:28 -0600

    Updated the App.js with AWS code

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import React from 'react';
 import styles from './App.module.css';
 import ChatComponent from './ChatComponent';
 import SignInForm from './SignInForm'; // Import the SignInForm component
+import { Amplify, withAuthenticator } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
 
-function App() {
+Amplify.configure(config);
+
+function App({ signOut, user }) {
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
         <h1>OpenAI Chat 'gpt-4-0125-preview' 128k Tokens</h1>
-        <SignInForm /> {/* Add the SignInForm component */}
+        {/* Display the username and sign out button if the user is signed in */}
+        {user && (
+          <>
+            <h2>Hello {user.username}</h2>
+            <button onClick={signOut}>Sign out</button>
+          </>
+        )}
+        {/* Display the SignInForm component if the user is not signed in */}
+        {!user && <SignInForm />}
         <ChatComponent />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
