5b79bb70e8a1682f61f80b3556c778cb7590c832
johame72 <johnhmerkel@gmail.com>
2024-02-17 14:07:56 -0600

    Made date the same H2 header as the usernemae

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ function App({ signOut, user }) {
       </header>
       {user && (
         <div style={{ margin: '0 7px', display: 'flex', alignItems: 'center' }}>
-          <h2 style={{ marginRight: '10px', margin: 0, fontSize: '1.5rem' }}>{user.username}'s Chat</h2>
-          <p style={{ margin: 0, fontSize: '1.5rem' }}>{currentDate}</p>
+          <h2 style={{ marginRight: '10px', margin: 0, fontSize: '1.5rem' }}>{user.username}'s Chat </h2>
+          <h2 style={{ margin: 0, fontSize: '1.5rem' }}>{currentDate}</h2>
         </div>
       )}
 
