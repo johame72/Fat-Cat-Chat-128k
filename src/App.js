8306936b53b68472bcccfb2f1e0310dbeff84fa5
johame72 <johnhmerkel@gmail.com>
2024-02-17 12:22:43 -0600

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
 import ChatComponent from './ChatComponent';
-import SignInForm from './SignInForm';
+import SignInForm from './SignInForm'; // Import the SignInForm component
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
-import chatIcon from './FatCatRobot.png';
+import chatIcon from './FatCatRobot.png'; // Import the chat icon
 
 Amplify.configure(config);
 
 function App({ signOut, user }) {
   const [apiKey, setApiKey] = useState('');
-  const currentDate = new Date().toISOString().split('T')[0];
+  const currentDate = new Date().toISOString().split('T')[0]; // Format: 'YYYY-MM-DD'
 
   return (
     <div className={styles.App}>
@@ -25,7 +25,10 @@ function App({ signOut, user }) {
         </div>
         {user && (
           <>
-            <h2 style={{ margin: '0 7px' }}>{user.username}'s Chat - {currentDate}</h2>
+            <div>
+              <h2 style={{ margin: '0 7px' }}>{user.username}'s Chat</h2>
+              <p style={{ margin: '0 7px' }}>{currentDate}</p>
+            </div>
             <input
               type="password"
               value={apiKey.replace(/./g, 'X')}
@@ -36,7 +39,7 @@ function App({ signOut, user }) {
           </>
         )}
         {!user && <SignInForm />}
-        <button onClick={signOut} style={{ border: '1px solid black' }}>Sign out</button>
+        <button onClick={signOut} style={{ border: '1px solid black', margin: '0 7px' }}>Sign out</button>
       </header>
       <ChatComponent apiKey={apiKey} />
     </div>
